dbb4e91db0b524f2092b462cbfbac6cae31a40a1
Krzysztof Urban <krzysztof.urban@firma.interia.pl>
2022-08-15 12:49:09 +0200

    feat(back|front): Created register logic

--- pages/register.tsx
+++ pages/register.tsx
@@ -4,25 +4,39 @@ import Input from 'components/atoms/Input/Input';
 import Button from 'components/atoms/Button/Button';
 import { useForm } from 'react-hook-form';
 import axios from 'axios';
+import ErrorMessage from 'components/atoms/ErrorMessage/ErrorMessage';
+import { Router, useRouter } from 'next/router';
 
-interface RegisterFormData {
+export interface RegisterFormData {
   name: string;
-  login: string;
+  surname: string;
+  email: string;
   password: string;
   repeatPassword: string;
 }
 
 const Register: NextPage = () => {
+  const router = useRouter();
   const {
     register,
     handleSubmit,
+    setError,
     formState: { errors },
   } = useForm();
-  const onSubmit = async (data: RegisterFormData): void => {
-    const response = await axios.post('/api/register', data);
-    console.log(response);
-  };
 
+  const onSubmit = async (formData: RegisterFormData): void => {
+    try {
+      const response = await axios.post('/api/register', formData);
+      const { message } = response.data;
+      if (message.error) {
+        setError(message.errorType, { type: 'custom', message: message.text });
+      } else {
+        router.push('/login');
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
   return (
     <div className="container">
       <Head>
@@ -35,11 +49,44 @@ const Register: NextPage = () => {
       </header>
       <main>
         <form onSubmit={handleSubmit(onSubmit)}>
-          <Input type="text" alt="name" placeholder="Podaj imię" label="name" {...register('name')} />
-          <Input type="text" alt="surname" placeholder="Podaj nazwisko" label="surname" {...register('name')} />
-          <Input type="email" alt="login" placeholder="Podaj login" label="login" {...register('login')} />
-          <Input type="password" alt="password" placeholder="Podaj hasło" label="password" {...register('password')} />
-          <Input type="password" alt="repeatPassword" placeholder="Powtórz hasło" label="Powtórz hasło" {...register('repeatPassword')} />
+          <Input type="text" alt="name" placeholder="Podaj imię" label="name" {...register('name', { required: 'Pole Imię jest wymagane' })} />
+          {errors.name && <ErrorMessage message={errors.name.message} />}
+          <Input
+            type="text"
+            alt="surname"
+            placeholder="Podaj nazwisko"
+            label="surname"
+            {...register('surname', { required: 'Pole Nazwisko jest wymagane' })}
+          />
+          {errors.surname && <ErrorMessage message={errors.surname.message} />}
+
+          <Input
+            type="email"
+            alt="email"
+            placeholder="Podaj e-mail"
+            label="email"
+            {...register('email', { required: 'Pole e-mail jest wymagane' })}
+          />
+          {errors.email && <ErrorMessage message={errors.email.message} />}
+
+          <Input
+            type="password"
+            alt="password"
+            placeholder="Podaj hasło"
+            label="password"
+            {...register('password', { required: 'Pole Hasło jest wymagane' })}
+          />
+          {errors.password && <ErrorMessage message={errors.password.message} />}
+
+          <Input
+            type="password"
+            alt="repeatPassword"
+            placeholder="Powtórz hasło"
+            label="Powtórz hasło"
+            {...register('repeatPassword', { required: 'Pole Powtórz hasło jest wymagane' })}
+          />
+          {errors.repeatPassword && <ErrorMessage message={errors.repeatPassword.message} />}
+
           <Button type="submit">Zarejestruj się!</Button>
         </form>
       </main>
