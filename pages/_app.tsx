5063177ddd3b0a0d915c2129554a6e9792437011
Krzysztof Urban <krzysztof.urban@firma.interia.pl>
2022-08-21 19:40:07 +0200

    feat(front): Add next-auth

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,15 +2,16 @@ import { theme } from 'assets/styles/theme';
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyle } from 'assets/styles/GlobalStyle';
+import { SessionProvider } from 'next-auth/react';
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
-    <>
+    <SessionProvider>
       <GlobalStyle />
       <ThemeProvider theme={theme}>
         <Component {...pageProps} />
       </ThemeProvider>
-    </>
+    </SessionProvider>
   );
 }
 
