9d114dee10c9b5ebfdc21a28744222332816f5d3
Krzysztof Urban <krzysztof.urban@firma.interia.pl>
2022-08-24 22:59:55 +0200

    fix(back): changed nexauth confg

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -6,13 +6,13 @@ import { PrismaAdapter } from '@next-auth/prisma-adapter';
 const prisma = new PrismaClient();
 
 export default NextAuth({
+  adapter: PrismaAdapter(prisma),
   providers: [
     FacebookProvider({
       clientId: process.env.FACEBOOK_CLIENT_ID as string,
       clientSecret: process.env.FACEBOOK_CLIENT_SECRET as string,
     }),
   ],
-  adapter: PrismaAdapter(prisma),
   secret: process.env.NEXTAUTH_SECRET,
   session: {
     strategy: 'jwt',
