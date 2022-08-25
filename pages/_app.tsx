274a759fb71cf559a1d7991d4e65cb52728c9f6b
Krzysztof Urban <krzychu222u@wp.pl>
2022-08-25 23:18:51 +0200

    fix(front): Added session props to SessionProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,7 @@ import { SessionProvider } from 'next-auth/react';
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
-    <SessionProvider>
+    <SessionProvider session={session}>
       <GlobalStyle />
       <ThemeProvider theme={theme}>
         <Component {...pageProps} />
