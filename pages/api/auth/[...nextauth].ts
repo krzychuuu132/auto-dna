57ae18a5abd9222e957d10842458b2620069fbe9
Krzysztof Urban <krzychu222u@wp.pl>
2022-08-25 23:09:00 +0200

    fix(back): Corrected models

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,9 +2,6 @@ import FacebookProvider from 'next-auth/providers/facebook';
 import { PrismaClient } from '@prisma/client';
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import NextAuth, { User as NextAuthUser } from 'next-auth';
-interface NextAuthUserWithStringId extends NextAuthUser {
-  id: string;
-}
 
 const prisma = new PrismaClient();
 
@@ -14,14 +11,6 @@ export default NextAuth({
     FacebookProvider({
       clientId: process.env.FACEBOOK_CLIENT_ID as string,
       clientSecret: process.env.FACEBOOK_CLIENT_SECRET as string,
-      profile(profile) {
-        return {
-          id: profile.id.toString(),
-          name: profile.name || profile.login,
-          email: profile.email,
-          image: profile.avatar_url,
-        } as NextAuthUserWithStringId;
-      },
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
